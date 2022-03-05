126ef8834a74945218a18ddb4fe781cfd6e0bae7
pradeepgiri <pradeepg78>
2022-03-05 14:14:08 -0500

    Finished

--- src/HackerRank.java
+++ src/HackerRank.java
@@ -1,14 +1,22 @@
 public class HackerRank {
     public static int hourGlass(int [][] arr)
     {
-        int max = 0;
-        int sum = 0;
+        int[][] sum = new int [4][4];
         for (int r = 0; r < arr.length - 2; r++)
         {
             for (int c = 0; c < arr[0].length - 2; c++)
             {
-                sum += arr[r][c] + arr[r][c + 1] + arr[r][c + 2] + arr[r + 1][c + 1] + arr[r + 2][c] + arr[r + 2][c + 1] + arr[r + 2][c + 2];
-                if (sum > max) max = sum;
+                int s = 0;
+                s += arr[r][c] + arr[r][c + 1] + arr[r][c + 2] + arr[r + 1][c + 1] + arr[r + 2][c] + arr[r + 2][c + 1] + arr[r + 2][c + 2];
+                sum[r][c] = s;
+            }
+        }
+        int max = sum[0][0];
+        for (int r = 0; r < sum.length; r++)
+        {
+            for (int c = 0; c < sum[0].length; c++)
+            {
+                if (sum[r][c] > max) max = sum[r][c];
             }
         }
         return max;
--- src/HackerRankRunner.java
+++ src/HackerRankRunner.java
@@ -5,13 +5,20 @@ public class HackerRankRunner {
                             {1, 1, 1, 0, 0, 0,},
                             {0, 0, 2, 4, 4, 0},
                             {0, 0, 0, 2, 0, 0,},
-                            {0, 0, 1, 2, 4, 0,},};
+                            {0, 0, 1, 2, 4, 0,},}; // should be 19
 
         int [][] testArr2 = {{-9, -9, -9, 1, 1, 1,},
                 {0, -9, 0, 4, 3, 2},
-                {1, 1, 1, 0, 0, 0,},
-                {0, 0, 2, 4, 4, 0},
-                {0, 0, 0, 2, 0, 0,},
-                {0, 0, 1, 2, 4, 0,},};
+                {-9, -9, -9, 1, 2, 3,},
+                {0, 0, 8, 6, 6, 0},
+                {0, 0, 0, -2, 0, 0,},
+                {0, 0, 1, 2, 4, 0,},}; //result should be 28
+
+        int[][] testArr3 = {{0, 1, 2, 3, 4, 5},{1, 2, 3, 4, 5, 6},{2, 3, 4, 5, 6, 7},{3, 4, 5, 6, 7, 8}, {4 , 5, 6, 7, 8, 9}, {5 , 6, 7, 8, 9, 9}};//answer should be 55
+
+        System.out.println(HackerRank.hourGlass(testArr));
+        System.out.println(HackerRank.hourGlass(testArr2));
+        System.out.println(HackerRank.hourGlass(testArr3));
+
     }
 }
